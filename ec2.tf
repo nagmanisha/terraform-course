259c00030b01b804e0fa75c6bba18906c57ab9dc
nagmanisha <nagmanisha25@gmail.com>
2025-03-05 16:33:22 +0530

    adding count

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,16 @@
 resource "aws_instance" "myec2" {
-  ami = "ami-123"
+  ami = "ami-05b10e08d247fb927"
   instance_type = "t2.micro"
   count = 3
+
+  tags = {
+    name = "payments-server-${count.index}"
+  }
   
 }
+
+resource "aws_iam_user" "that" {
+    name = "payments-user-${count.index}"
+    count = 3
+}
+  
